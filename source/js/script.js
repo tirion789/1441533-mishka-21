5d033af30d1652b4db7d2fb57c4aa1410b6372ae
tirion <tirionchen@mail.ru>
2021-01-26 01:38:01 +0300

    Правочки

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,24 @@
-const mainnav=document.querySelector(".main-navigation");
+
+const overlay = document.querySelector('.overlay');
+const  modal = document.querySelector('.modal-cart');
+const buttons = document.querySelectorAll('.js-button');
+
+if (buttons) {
+  for (let button of buttons) {
+    button.addEventListener('click', function (event) {
+      event.preventDefault();
+      modal.classList.add('modal-cart--show');
+      overlay.classList.add('overlay--show');
+    })
+  }
+}
+
+if (overlay) {
+  overlay.addEventListener('click', function (event) {
+  event.preventDefault();
+    if (overlay.classList.contains('overlay--show')) {
+      modal.classList.remove('modal-cart--show');
+      overlay.classList.remove('overlay--show');
+    }
+  })
+}
